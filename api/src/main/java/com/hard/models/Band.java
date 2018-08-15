aa75842768433e5b7ecc611413c93e4705fb64aa
hard <harleydav@mail.ru>
2018-08-15 19:19:15 +0300

    Band model

--- api/src/main/java/com/hard/models/Band.java
+++ api/src/main/java/com/hard/models/Band.java
@@ -1,14 +1,8 @@
 package com.hard.models;
 
-public class Song {
+public class Band {
     private long id;
-    private Band band;
     private String title;
-
-    public Song(Band band, String title) {
-        this.band = band;
-        this.title = title;
-    }
 
     public long getId() {
         return id;
@@ -16,14 +10,6 @@ public class Song {
 
     public void setId(long id) {
         this.id = id;
-    }
-
-    public Band getBand() {
-        return band;
-    }
-
-    public void setBand(Band band) {
-        this.band = band;
     }
 
     public String getTitle() {
