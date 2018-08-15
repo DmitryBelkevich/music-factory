6682d21a222194e6e900069de4f800eadcd055d9
hard <harleydav@mail.ru>
2018-08-15 19:13:30 +0300

    SongRepository

--- webapp/src/main/java/com/hard/services/impl/SongService.java
+++ webapp/src/main/java/com/hard/services/impl/SongService.java
@@ -1,34 +1,20 @@
 package com.hard.services.impl;
 
-import com.hard.models.Band;
 import com.hard.models.Song;
+import com.hard.repositories.SongRepository;
 import com.hard.services.IService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.Collection;
 
 @Service
 public class SongService implements IService<Song> {
+    @Autowired
+    private SongRepository songRepository;
+
     @Override
     public Collection<Song> getAll() {
-        Band band = new Band();
-
-        band.setTitle("band 1");
-
-        Collection<Song> songs = new ArrayList<>();
-
-        songs.add(new Song(band, "song 1"));
-        songs.add(new Song(band, "song 2"));
-        songs.add(new Song(band, "song 3"));
-        songs.add(new Song(band, "song 4"));
-        songs.add(new Song(band, "song 5"));
-        songs.add(new Song(band, "song 6"));
-        songs.add(new Song(band, "song 7"));
-        songs.add(new Song(band, "song 8"));
-        songs.add(new Song(band, "song 9"));
-        songs.add(new Song(band, "song 10"));
-
-        return songs;
+        return songRepository.getAll();
     }
 }
