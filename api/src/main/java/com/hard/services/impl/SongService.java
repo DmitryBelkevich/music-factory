45337564e197c6d8deb84d4c32884ac7643f39ac
hard <harleydav@mail.ru>
2018-08-15 19:21:14 +0300

    SongService

--- api/src/main/java/com/hard/services/impl/SongService.java
+++ api/src/main/java/com/hard/services/impl/SongService.java
@@ -1,22 +1,18 @@
 package com.hard.services.impl;
 
 import com.hard.models.Song;
+import com.hard.repositories.SongRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.Collection;
 
 @Service
 public class SongService {
+    @Autowired
+    private SongRepository songRepository;
+
     public Collection<Song> getAll() {
-        Collection<Song> songs = new ArrayList<>();
-
-        songs.add(new Song());
-        songs.add(new Song());
-        songs.add(new Song());
-        songs.add(new Song());
-        songs.add(new Song());
-
-        return songs;
+        return songRepository.getAll();
     }
 }
