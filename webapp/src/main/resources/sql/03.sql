636212a36c85ef77e8a08b1ca7c92fb7c145fd41
hard <harleydav@mail.ru>
2020-01-13 12:19:20 +0300

    added band

--- webapp/src/main/resources/sql/03.sql
+++ webapp/src/main/resources/sql/03.sql
@@ -95,7 +95,8 @@ VALUES (1, 'Ace of Base', '1990', '2012'),
        (66, 'Animal Джаz', '2000', NULL),
        (67, 'Михаил Боярский', '1983', NULL),
        (68, 'Сектор Газа', '1987', '2000'),
-       (69, 'АукцЫон', '1978', NULL);
+       (69, 'АукцЫон', '1978', NULL),
+       (70, 'N.R.M.', '1994', NULL);
 
 INSERT INTO bands_countries_relations (band_id, country_id)
 VALUES (1, 6),
