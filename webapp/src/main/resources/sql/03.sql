8f69360ee9fd9f3cc1dea064e0490bbafbb76c9e
hard <harleydav@mail.ru>
2020-06-10 00:05:53 +0300

    added song

--- webapp/src/main/resources/sql/03.sql
+++ webapp/src/main/resources/sql/03.sql
@@ -354,4 +354,5 @@ VALUES (1, 1, 'All That She Wants', 4, NULL),
        (151, 77, 'Pretty Fly (for a White Guy)', 2, NULL),
        (152, 61, 'Karma Police', 1, NULL),
        (153, 77, 'Self Esteem', 0, NULL),
-       (154, 78, 'Supergirl', 3, NULL);
+       (154, 78, 'Supergirl', 3, NULL),
+       (155, 70, 'Лёгкiя-лёгкiя', -1, NULL);
